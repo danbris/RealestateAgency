0f9e9aca98b51e08ce0c2d67e863021e987bcb99
Sirb Daniel <daniel.sirb@accesa.eu>
2019-12-05 15:27:19 +0200

    Initial Commit

---
+++ Residence.ViewModels/StudioViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Residence.ViewModels
+{
+    public class StudioViewModel: ViewModelBase
+    {
+        private int _number;
+        private int _floorNumber;
+
+        public int FloorNumber
+        {
+            get => _floorNumber;
+            set => SetValue(ref _floorNumber, value, nameof(FloorNumber));
+        }
+    }
+}
---
+++ ResidenceShoppe/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Residence.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResidenceShoppe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+            }
+        }
+
+        public StudioViewModel StudioVM { get; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StudioVM = new StudioViewModel();
+            DataContext = StudioVM;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Text += DateTime.Now.ToShortDateString();
+            StudioVM.FloorNumber += 1;
+        }
+    }
+}
